07052b28daa5612c1d01d58fa71279b3342ad97a
Magnus Norddahl <dpjudas@users.noreply.github.com>
2020-10-29 22:28:38 +0100

    Enable anisotropy

--- VulkanDrv/SceneSamplers.cpp
+++ VulkanDrv/SceneSamplers.cpp
@@ -11,11 +11,13 @@ SceneSamplers::SceneSamplers(VulkanDevice *device)
 	builder.setMinFilter(VK_FILTER_LINEAR);
 	builder.setMagFilter(VK_FILTER_LINEAR);
 	builder.setMipmapMode(VK_SAMPLER_MIPMAP_MODE_LINEAR);
+	builder.setAnisotropy(8.0);
 	repeat = builder.create(device);
 
 	builder.setAddressMode(VK_SAMPLER_ADDRESS_MODE_REPEAT);
 	builder.setMinFilter(VK_FILTER_NEAREST);
 	builder.setMagFilter(VK_FILTER_NEAREST);
 	builder.setMipmapMode(VK_SAMPLER_MIPMAP_MODE_NEAREST);
+	builder.setAnisotropy(8.0);
 	nosmooth = builder.create(device);
 }
