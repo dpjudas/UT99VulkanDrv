894cfc96eaae3678adfcb8ea6b003f12c77b16df
Magnus Norddahl <dpjudas@users.noreply.github.com>
2022-07-28 15:02:06 +0200

    Use std::unordered_map instead of std::map for slightly faster cache lookups

--- VulkanDrv/DescriptorSetManager.h
+++ VulkanDrv/DescriptorSetManager.h
@@ -6,6 +6,44 @@
 class UVulkanRenderDevice;
 class VulkanTexture;
 
+struct TexDescriptorKey
+{
+	TexDescriptorKey(VulkanTexture* tex, VulkanTexture* lightmap, VulkanTexture* detailtex, VulkanTexture* macrotex, uint32_t sampler) : tex(tex), lightmap(lightmap), detailtex(detailtex), macrotex(macrotex), sampler(sampler) { }
+
+	bool operator==(const TexDescriptorKey& other) const
+	{
+		return tex == other.tex && lightmap == other.lightmap && detailtex == other.detailtex && macrotex == other.macrotex && sampler == other.sampler;
+	}
+
+	bool operator<(const TexDescriptorKey& other) const
+	{
+		if (tex != other.tex)
+			return tex < other.tex;
+		else if (lightmap != other.lightmap)
+			return lightmap < other.lightmap;
+		else if (detailtex != other.detailtex)
+			return detailtex < other.detailtex;
+		else if (macrotex != other.macrotex)
+			return macrotex < other.macrotex;
+		else
+			return sampler < other.sampler;
+	}
+
+	VulkanTexture* tex;
+	VulkanTexture* lightmap;
+	VulkanTexture* detailtex;
+	VulkanTexture* macrotex;
+	uint32_t sampler;
+};
+
+template<> struct std::hash<TexDescriptorKey>
+{
+	std::size_t operator()(const TexDescriptorKey& k) const
+	{
+		return (((std::size_t)k.tex ^ (std::size_t)k.lightmap));
+	}
+};
+
 class DescriptorSetManager
 {
 public:
@@ -43,32 +81,7 @@ private:
 	std::vector<std::unique_ptr<VulkanDescriptorPool>> SceneDescriptorPool;
 	int SceneDescriptorPoolSetsLeft = 0;
 
-	struct TexDescriptorKey
-	{
-		TexDescriptorKey(VulkanTexture* tex, VulkanTexture* lightmap, VulkanTexture* detailtex, VulkanTexture* macrotex, uint32_t sampler) : tex(tex), lightmap(lightmap), detailtex(detailtex), macrotex(macrotex), sampler(sampler) { }
-
-		bool operator<(const TexDescriptorKey& other) const
-		{
-			if (tex != other.tex)
-				return tex < other.tex;
-			else if (lightmap != other.lightmap)
-				return lightmap < other.lightmap;
-			else if (detailtex != other.detailtex)
-				return detailtex < other.detailtex;
-			else if (macrotex != other.macrotex)
-				return macrotex < other.macrotex;
-			else
-				return sampler < other.sampler;
-		}
-
-		VulkanTexture* tex;
-		VulkanTexture* lightmap;
-		VulkanTexture* detailtex;
-		VulkanTexture* macrotex;
-		uint32_t sampler;
-	};
-
-	std::map<TexDescriptorKey, std::unique_ptr<VulkanDescriptorSet>> TextureDescriptorSets;
+	std::unordered_map<TexDescriptorKey, std::unique_ptr<VulkanDescriptorSet>> TextureDescriptorSets;
 
 	std::unique_ptr<VulkanDescriptorPool> PresentDescriptorPool;
 	std::unique_ptr<VulkanDescriptorSet> PresentDescriptorSet;
